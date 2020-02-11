349e22522dc7b8c74780070d601479458782d308
kjacasio <59855973+kjacasio@users.noreply.github.com>
2020-02-11 10:51:11 -1000

    added  third view controller Feb 11

---
+++ HackwichFour/thirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  thirdViewController.swift
+//  HackwichFour
+//
+//  Created by kat on 2/11/20.
+//  Copyright © 2020 kat. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
